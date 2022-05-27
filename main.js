9254c8e94166b347f4b294152463146b3eee4c78
Andrii Torkotiuk <torkotiuk@gmail.com>
2022-05-28 00:50:29 +0200

    add some animation

--- main.js
+++ main.js
@@ -1,32 +1,22 @@
-// Get modal element
-const modal = document.getElementById('simpleModal');
-// Get open modal button
-const modalBtn = document.getElementById('modalBtn');
-// Get close button
+const backdrop = document.getElementById('simpleBackdrop');
+const backdropBtn = document.getElementById('backdropBtn');
 const closeBtn = document.getElementsByClassName('closeBtn')[0];
 
-// Listen for open click
-modalBtn.addEventListener('click', openModal);
-// Listen for close click
-closeBtn.addEventListener('click', closeModal);
-// Listen for outside click
+backdropBtn.addEventListener('click', openBackdrop);
+closeBtn.addEventListener('click', closeBackdrop);
 window.addEventListener('click', clickOutside);
 
-// FN to open modal
-function openModal() {
-  modal.style.display = 'block';
+function openBackdrop() {
+  backdrop.style.display = 'block';
 }
 
-// FN to close modal
-function closeModal() {
-  modal.style.display = 'none';
+function closeBackdrop() {
+  backdrop.style.display = 'none';
 }
 
-// FN to close modal if outside click
 function clickOutside(e) {
-  // 'modal' - is dark area around
   console.log('clickOutside DOES NOT working correctly!!');
-  if (e.target == modal) {
-    modal.style.display = 'none';
+  if (e.target == backdrop) {
+    backdrop.style.display = 'none';
   }
 }
